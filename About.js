a5d4586efcce87e785c879d1723944bb88ed9941
Pavan-723 <venkatpavan099@gmail.com>
2024-07-12 12:24:00 +0530

    pavanportfolio

---
+++ About.js
@@ -0,0 +1,15 @@
+// src/pages/About.js
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about">
+      <h1>About Me</h1>
+      <p>Name:pavan</p>
+      <p>Email:venkatpavan099@gmail.com</p>
+      <p>Mobile:9063740720</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ Contact.js
@@ -0,0 +1,15 @@
+// src/pages/Contact.js
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="contact">
+      <h1>Contact</h1>
+      <p>Please feel free to reach out to me via email or phone.</p>
+      <p>Email:venkatpavan099@gmail.com</p>
+      <p>Phone:9063740720</p>
+    </div>
+  );
+}
+
+export default Contact;
